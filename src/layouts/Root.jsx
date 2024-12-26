650c524c108d838f4fd6beef2faf678521b688ae
hrmrakib <hrmrakibs@gmail.com>
2024-12-26 18:08:19 +0600

    h

--- src/layouts/Root.jsx
+++ src/layouts/Root.jsx
@@ -4,7 +4,8 @@ import Navbar from "./../components/header/Navbar";
 
 const Root = () => {
   return (
-    <div className='bg-[#111216]'>
+    <div className='bg-[#111216]'> 
+    
       <Navbar />
       <Outlet />
       <Footer />
